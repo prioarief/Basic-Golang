d0c5af6ab149815d82faece2b6a0a9240e6bd66a
prioarief <prioariefgunawangunawan@gmail.com>
2020-10-16 10:46:48 +0700

    map on golang

---
+++ 08-map/main.go
@@ -0,0 +1,46 @@
+package main
+
+func main() {
+	// var chicken map[string]int // key = string || value = int
+	// chicken = map[string]int{} // explisit agar tidak nil
+	// chicken["prio"] = 17
+	// fmt.Println(chicken["prio"])
+
+	// // cara vertikal
+	// var chicken1 = map[string]int{"januari": 50, "februari": 40}
+
+	// // cara horizontal
+	// var chicken2 = map[string]int{
+	// 	"januari":  50,
+	// 	"februari": 40,
+	// }
+
+	// // ITERASI
+	// var chicken = map[string]int{
+	// 	"januari":  50,
+	// 	"februari": 40,
+	// 	"maret":    34,
+	// 	"april":    67,
+	// }
+
+	// for key, val := range chicken {
+	// 	fmt.Println(key, "  \t:", val)
+	// }
+
+	// // DELETE ITEM MAP
+	// var chicken = map[string]string{"name": "prio", "age": "18"}
+	// for _, val := range chicken {
+	// 	fmt.Println(val)
+	// }
+	// delete(chicken, "age")
+	// fmt.Println("AFTER DELETE")
+	// for _, val := range chicken {
+	// 	fmt.Println(val)
+	// }
+
+	// // CHECK IF ITEM IS EXIST
+	// var chicken = map[string]string{"name": "prio", "age": "18"}
+	// value, isExist := chicken["name"]
+	// fmt.Println(value, isExist) // isExist true
+
+}
