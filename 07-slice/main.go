f1861ac82c06b375402205b4ed45258ef55425b3
prio.gunawan <prio.gunawan@kanggo.id>
2022-01-21 14:21:16 +0700

    learn slice deeply

--- 07-slice/main.go
+++ 07-slice/main.go
@@ -5,16 +5,24 @@ import (
 )
 
 func main() {
-	// Slice adalah reference elemen array. Slice bisa dibuat, atau bisa juga dihasilkan dari manipulasi sebuah array ataupun slice lainnya. Karena merupakan data reference, menjadikan perubahan data di tiap elemen slice akan berdampak pada slice lain yang memiliki alamat memori yang sama.
+	// Slice adalah reference elemen array. Slice bisa dibuat, atau bisa juga dihasilkan dari manipulasi sebuah array ataupun slice lainnya. Karena merupakan data reference, menjadikan perubahan data di tiap elemen slice akan berdampak pada slice lain yang memiliki alamat memori yang sama dan sebaliknya.
 
 	// var fruitsA = []string{"apple", "grape"}     // slice
 	// var fruitsB = [2]string{"banana", "melon"}   // array
 	// var fruitsC = [...]string{"papaya", "grape"} // array
 
-	var fruits = [...]string{"Apple", "Banana", "Orange"}
-	var newFruitss = fruits[:] // slice all elements
+	var fruits = [...]string{"Apple", "Banana", "Orange", "Manggo"}
+	var newFruitss = fruits[:2] // slice all elements
 	// var newFruits = fruits[0:2] // slice from index 0 - before index 2
 	// fmt.Println(newFruits) // [Apple Banana]
-	fmt.Println(newFruitss) // [Apple Banana Orange]
+	fmt.Println(newFruitss)      // [Apple Banana]
+	fmt.Println(cap(newFruitss)) // mendapatkan kapasitas dari slice tersebut (4)
+	fmt.Println(len(newFruitss)) // mendapatkan length dari slice (2)
+
+	// append slice
+	var fruit2 = append(newFruitss, "new")
+	fmt.Println(fruit2)
+	fmt.Println(newFruitss)
+	fmt.Println(fruits)
 
 }
