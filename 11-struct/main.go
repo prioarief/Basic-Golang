f18993411796ae77361468f3dc3d4d69c32792e4
Prio Arief Gunawan <prio.gunawan@kanggo.id>
2023-01-11 14:24:31 +0700

    learn struct

--- 11-struct/main.go
+++ 11-struct/main.go
@@ -1,48 +1,98 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type student struct {
 	name  string
 	grade int
 }
 
-type class struct {
-	className string
-	student
+type newstudent struct {
+	grade int
+	person
+}
+type person struct {
+	name string
+	age  int
 }
 
+type alias_person = person
+
+// type person_horizontal struct { name string; age int }
+
 func main() {
-	// fmt.Println("Hi from struct")
-	// deklarasi struct
-
-	// var s1 student // inisialisasi 1
-	// var s1 = student{} // inisialisasi 2
-	// s1.name = "Prio"
-	// s1.grade = 18
-
-	// var s1 = student{"Prio", 18} // inisialisasi 3
-	// fmt.Println(s1)
-
-	// Object Pointer
-	// var s1 = student{name: "Prio", grade: 18}
-	// var s2 *student = &s1
-
-	// fmt.Println("student 1, name :", s1.name)
-	// fmt.Println("student 4, name :", s2.name)
-
-	// s2.name = "Arief"
-	// fmt.Println("student 1, name :", s1.name)
-	// fmt.Println("student 4, name :", s2.name)
-
-	// embedded struct
-	var s1 = class{}
-	s1.className = "RPL"
+	var s1 student
 	s1.name = "Prio"
-	s1.grade = 18
-	fmt.Println(s1.student.name)
-	fmt.Println(s1.name)
-	fmt.Println(s1.className)
-	fmt.Println(s1.grade)
 
+	var s2 = student{}
+	s2.name = "Arief"
+
+	// var s3 = student{"Gunawan"} return error
+	var s3 = student{"Gunawan", 5}
+	var s4 = student{name: "Prio Arief Gunawan"}
+
+	fmt.Println(s1, "s1")
+	fmt.Println(s2, "s2")
+	fmt.Println(s3, "s3")
+	fmt.Println(s4, "s4")
+
+	var s5 = student{name: "John", grade: 10}
+	var s6 *student = &s5
+
+	s6.name = "Yoga"
+
+	fmt.Println(s5, "s5")
+	fmt.Println(s6, "s6")
+
+	var s7 = newstudent{}
+	s7.age = 21
+	s7.name = "Mbappe"
+	s7.grade = 10
+
+	fmt.Println(s7)
+
+	// anonymous struct = cocok untuk sekali pakai
+	var s8 = struct {
+		name string
+		age  int
+	}{
+		name: "John",
+		age:  20,
+	}
+
+	var s9 = struct {
+		name string
+		age  int
+	}{}
+
+	var s10 struct {
+		name string
+		age  int
+	}
+
+	s9.age = 20
+	s9.name = "Messi"
+
+	s10.age = 20
+	s10.name = "Ronaldo"
+
+	fmt.Println(s8)
+	fmt.Println(s9)
+	fmt.Println(s10)
+
+	// slice with struct
+	var students = []student{
+		{name: "John", grade: 10},
+		{name: "John", grade: 10},
+		{name: "John", grade: 10},
+	}
+
+	for _, e := range students {
+		fmt.Println("My name is", e.name, "I'm", e.grade)
+	}
+
+	var p1 = alias_person{name: "Saya", age: 22}
+	fmt.Println(p1)
 }
