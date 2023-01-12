6e82c00162869f60fb0fff85a59a68e312ba5529
Prio Arief Gunawan <prio.gunawan@kanggo.id>
2023-01-12 10:59:55 +0700

    learn export import

--- 13-exported/main.go
+++ 13-exported/main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	fmt.Println("Hello")
+	fmt.Println(helper.Version)
 	helper.SayHello()
 
 }
--- helper/main.go
+++ helper/main.go
@@ -5,3 +5,7 @@ import "fmt"
 func SayHello() {
 	fmt.Println("SayHello From Package helper has been called")
 }
+
+var Version = "2.9"
+
+// untuk export harus menggunakan kapital di awal nama variabel
